847a07de736f34dacc35b1cb55592f36e002c7cf
Vakada Vamsi <vamsivakada163@gmail.com>
2024-07-11 17:01:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+console.log("Hello from script.js");
+
+// Create an h1 element
+var h1 = document.createElement("h1");
+h1.textContent = "Hello, World!";
+
+// Append the h1 element to the body
+document.body.appendChild(h1);
