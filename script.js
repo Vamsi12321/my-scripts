657020551932b4dd030f811772d6a3da32e40def
Vamsi12321 <144164056+Vamsi12321@users.noreply.github.com>
2024-07-11 17:10:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // script.js
-console.log("Hello from script.js");
+console.log('Hello from script.js');
+
+// Find the existing element by its ID
+var targetDiv = document.getElementById('div-proxzar-getresp');
 
 // Create an h1 element
-var h1 = document.createElement("h1");
-h1.textContent = "Hello, World!";
+var h1 = document.createElement('h1');
+h1.textContent = 'Hello, Vamsi';
 
-// Append the h1 element to the body
-document.body.appendChild(h1);
+// Append the h1 element to the targetDiv
+targetDiv.appendChild(h1);
